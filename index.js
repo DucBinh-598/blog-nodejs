cd555bcda3928552e0c43cc91f3ab03efeb61d56
DucBinh-598 <afkpixeldb598@gmail.com>
2026-07-15 10:11:07 +0700

    Khởi tạo dự án Express cơ bản

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 const express = require('express');
 const morgan = require('morgan'); // Khai báo morgan
+const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
-const { engine } = require('express-handlebars');
+app.use(morgan('combined')); 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.engine('hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
-app.set('views', './views'); 
-app.use(morgan('combined'));
+app.set('views', './views');
 app.get('/', (req, res) => {
     res.render('home');
 });
-// Route xử lý cho trang About
 app.get('/about', (req, res) => {
     res.render('about');
 });
-
-// Route xử lý cho trang Liên hệ
 app.get('/contact', (req, res) => {
     res.render('contact');
 });
-
-// Route hiển thị trang Tìm kiếm
 app.get('/search', (req, res) => {
     res.render('search');
 });
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
+});
+app.post('/blogs/create', (req, res) => {
+    console.log("Dữ liệu nhận được từ Form: ", req.body);
+    res.json(req.body);
+});
 app.listen(port, () => {
-  console.log(`Server đang chạy tại http://localhost:${port}`);
+    console.log(`Server đang chạy tại http://localhost:${port}`);
 });
